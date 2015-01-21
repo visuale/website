a62967635a1b25e112265416f33fc23b80722ef9
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2015-01-21 14:13:52 -0500

    Update SiteConstants.java

--- src/main/java/org/jboss/forge/website/SiteConstants.java
+++ src/main/java/org/jboss/forge/website/SiteConstants.java
@@ -12,7 +12,7 @@ package org.jboss.forge.website;
 public interface SiteConstants
 {
    String SH_SCRIPT_URL = "https://raw.githubusercontent.com/forge/core/master/forge-install.sh";
-   String REPO_BASE_URL = "https://github.com/forge/website-data/raw/master";
+   String REPO_BASE_URL = "https://raw.githubusercontent.com/forge/website-data/master";
 
    String ADDON_REPO_URL_CORE = REPO_BASE_URL + "/addons-core.yaml";
    String ADDON_REPO_URL_COMMUNITY = REPO_BASE_URL + "/addons-community.yaml";
